cb36223fa2fd8ecb94730f782730f0829b9a5146
Diego GP <diego.gp227@gmail.com>
2026-08-03 10:29:55 -0500

    test: url test

--- front/src/shared/constants/urls.ts
+++ front/src/shared/constants/urls.ts
@@ -1,7 +1,5 @@
-// export const BaseURL =
-//   process.env.NEXT_PUBLIC_API_URL ?? "http://localhost:8000/api/";
-export const BaseURL ="https://api-critvirgilio.devdiego.work/api/";
-
+export const BaseURL =
+  process.env.NEXT_PUBLIC_API_URL ?? "http://localhost:8000/api/";
 
 export const LoginURL = new URL("login", BaseURL);
 export const SignupURL = new URL("signup", BaseURL);
