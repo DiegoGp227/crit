b32290d25f2c64f1ab633c9519619beda3b0a418
Diego GP <diego.gp227@gmail.com>
2026-08-26 14:04:42 -0500

    fix: sponsors posicion corrected

--- front/src/features/home/components/organisms/SponsorsSection.tsx
+++ front/src/features/home/components/organisms/SponsorsSection.tsx
@@ -12,14 +12,14 @@ interface Sponsor {
 
 const sponsors: Sponsor[] = [
     { name: "Sumerian Cycling", logo: "/brand/sponsors/Sumerian4-09.png", url: "https://www.instagram.com/sumeriancycling/", gold: true },
-    { name: "Bicicle Parking", logo: "/brand/sponsors/Bicilcle Parking.png", url: "https://www.instagram.com/bicycle_parking.r/", gold: true },
-    { name: "The Bike Thender", logo: "/brand/sponsors/The Bike Thender_Mesa de trabajo 1.png", url: "https://www.instagram.com/thebikethender/", gold: true },
     {
         name: "Bogofija / BogoChicas",
         logos: ["/brand/sponsors/Logo Bogofija 2021.png", "/brand/sponsors/BogoChicas TM.png"],
         url: "https://www.instagram.com/bogofija/",
         gold: true,
     },
+    { name: "Bicicle Parking", logo: "/brand/sponsors/Bicilcle Parking.png", url: "https://www.instagram.com/bicycle_parking.r/", gold: true },
+    { name: "The Bike Thender", logo: "/brand/sponsors/The Bike Thender_Mesa de trabajo 1.png", url: "https://www.instagram.com/thebikethender/", gold: true },
 ];
 
 function InstagramIcon({ className }: { className?: string }) {
