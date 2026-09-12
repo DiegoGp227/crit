6440302af6a02a652bd119825b5e316a70d7b1b1
Diego GP <diego.gp227@gmail.com>
2026-09-12 18:27:28 -0500

    feat: new sponsor agregue

--- front/src/features/home/components/organisms/SponsorsSection.tsx
+++ front/src/features/home/components/organisms/SponsorsSection.tsx
@@ -24,6 +24,7 @@ const sponsors: Sponsor[] = [
     { name: "Bicicle Parking", logo: "/brand/sponsors/Bicilcle Parking.png", url: "https://www.instagram.com/bicycle_parking.r/", gold: true },
     { name: "Rolling Props", logo: "/brand/sponsors/Rolling Props.png", url: "https://www.instagram.com/rolling_props?stkn=d2o3MDU2aGhkaXc=", gold: true },
     { name: "Hanei", logo: "/brand/sponsors/Hanei.png", url: "https://www.instagram.com/haneiintegral", gold: true },
+    { name: "Urban", logo: "/brand/sponsors/URBAN.png", url: "https://www.instagram.com/urban3d.sv/", gold: true },
 ];
 
 function InstagramIcon({ className }: { className?: string }) {
